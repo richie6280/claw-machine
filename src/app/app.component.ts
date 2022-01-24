0a954805f3f34a738b71c19efd9fe12dfdca85c3
Richie <richie@Jeande-MacBook-Pro.local>
2022-01-24 14:34:27 +0800

    add claw bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,8 @@ export class AppComponent {
   win: boolean = false;
 
   startEventLeft(e: any): void {
-    if(this.x < 0) return;
-    const claw = document.querySelector('.crane-arm-claw') as HTMLElement;
+    if (this.x < 0) return;
+    const claw = document.querySelector('.claw') as HTMLElement;
     this.count = setInterval(() => {
       this.x--;
       claw.style.transform = `translateX(${this.x}px)`;
@@ -30,12 +30,12 @@ export class AppComponent {
 
   startEventRight(e: any): void {
     const machine = document.querySelector('.craneGame') as HTMLElement;
-    const claw = document.querySelector('.crane-arm-claw') as HTMLElement;
-    if(this.x > machine.offsetWidth - 53) return;
+    const claw = document.querySelector('.claw') as HTMLElement;
+    if (this.x > machine.offsetWidth - 100) return;
     this.count = setInterval(() => {
       this.x++;
       claw.style.transform = `translateX(${this.x}px)`;
-      if (this.x > (machine.offsetWidth - 53)) {
+      if (this.x > (machine.offsetWidth - 100)) {
         clearInterval(this.count);
       };
     }, 3);
@@ -49,28 +49,46 @@ export class AppComponent {
 
   grab() {
     const machine = document.querySelector('.craneGame') as HTMLElement;
-    const claw = document.querySelector('.crane-arm-claw') as HTMLElement;
-    this.count = setInterval(() => {
-      this.y++;
-      claw.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
+    const claw = document.querySelector('.claw') as HTMLElement;
+    const clawBar = document.querySelector('.clawBar') as HTMLElement;
+    const armClaw = document.querySelector('.crane-arm-claw') as HTMLElement;
+    const leftClaw = document.querySelector('.leftClaw') as HTMLElement;
+    const rightClaw = document.querySelector('.rightClaw') as HTMLElement;
 
-      if (this.y > (machine.offsetHeight - 52)) {
+    this.count = setInterval(() => {
+      leftClaw.classList.add('clawGrab');
+      rightClaw.classList.add('clawGrab');
+      this.y++;
+      armClaw.style.transform = `translate3d(${0}px,${this.y}px, 0)`;
+      clawBar.style.height = `${this.y + 15}px`;
+
+      if (this.y > (machine.offsetHeight - 100)) {
         clearInterval(this.count);
         this.getPrize();
         this.count = setInterval(() => {
+          leftClaw.classList.remove('clawGrab');
+          rightClaw.classList.remove('clawGrab');
           this.y--;
-          claw.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
+          armClaw.style.transform = `translate3d(${0}px,${this.y}px, 0)`;
+          clawBar.style.height = `${this.y + 15}px`;
 
           if (this.y < 0) {
             clearInterval(this.count);
-            if (!this.win) { this.dropPrize() } //未中獎掉禮物
+            if (!this.win) this.dropPrize() //未中獎掉禮物
             this.count = setInterval(() => {
               this.x--;
               claw.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
-              
+
               if (this.x < 0) {
+                leftClaw.classList.add('clawGrab');
+                rightClaw.classList.add('clawGrab');
+                setTimeout(() => {
+                  leftClaw.classList.remove('clawGrab');
+                  rightClaw.classList.remove('clawGrab');
+                }, 250)
+
                 clearInterval(this.count);
-                if (this.win) { this.dropPrize() }  //中獎掉禮物
+                if (this.win) this.dropPrize();  //中獎掉禮物
               };
 
             });
@@ -90,6 +108,10 @@ export class AppComponent {
     prize.style.width = '25px';
     prize.style.height = '25px';
     prize.style.background = 'red';
+    prize.style.borderRadius = '50%';
+    prize.style.position = 'absolute';
+    prize.style.top = '72px';
+    prize.style.left = '18px';
   }
 
   dropPrize() {
@@ -101,18 +123,21 @@ export class AppComponent {
     this.count = setInterval(() => {
       if (!this.win) { //未中獎
         prizeY++;
-        prize.style.transform = `translate3d(${prizeX-this.x}px,${prizeY}px, 0)`;
+        prize.style.transform = `translate3d(${prizeX - this.x}px,${prizeY}px, 0)`;
       } else {  //中獎
         this.y++;
         prize.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
       }
 
-      let disappear = machine.offsetHeight - 25;
+      let disappear = machine.offsetHeight - 100;
       if (this.y > disappear || prizeY > disappear) {
         prize.style.display = 'none';
       }
     }, 3);
   }
 
+  change() {
+    this.win = !this.win;
+  }
 
 }
