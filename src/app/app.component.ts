a5e99fef79cfca7db1d42fe8ec839659e6aabc04
Richie <richie@Jeande-MacBook-Pro.local>
2022-02-09 17:19:51 +0800

    btn animation、style change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.swingRight();
-    // this.customizationImages(); 
+    this.customizationImages(); 
   }
 
   constructor() { }
@@ -18,6 +18,12 @@ export class AppComponent {
   x: number = 0;
   y: number = 0;
   win: boolean = true;
+
+  //扭蛋 禮物 彩球
+  style: string = '扭蛋';
+  //客製化可選圖片
+  banner: string = '#fff';
+  icon: string = '';
 
   // startEventLeft(e: any): void {
   //   const claw = document.querySelector('.claw') as HTMLElement;
@@ -63,32 +69,52 @@ export class AppComponent {
   // }
 
   customizationImages() {
+    //風格
     const banner = document.querySelector('.banner') as HTMLElement;
-    const background = document.querySelector('.container') as HTMLElement;
+    const machine = document.querySelector('.machine') as HTMLElement;
+    const prizes = document.querySelector('.prizes') as HTMLElement;
+    const joystick = document.querySelector('.joystick') as HTMLElement;
+    const getImg = document.querySelector('.get img') as HTMLElement;
+    const get = document.querySelector('.get') as HTMLElement;
+
+    //客製化圖片
+    const background = document.querySelector('.background') as HTMLElement;
     const icon = document.querySelector('.icon') as HTMLElement;
-    banner.style.position = 'absolute';
-    banner.style.top = '7.3vw';
-    banner.style.width = '85.5vw';
-    banner.style.height = '24.6vw';
-    banner.style.background = '#000';
-    background.style.zIndex = '-5'
-    background.style.background = '#ccc'
-    icon.style.zIndex = '3';
-    icon.style.position = 'absolute';
-    icon.style.top = '36.5vw';  
-    icon.style.width = '80vw';
-    icon.style.height = '42vw';
-    icon.style.background = 'red';
+
+    if(this.style === '扭蛋') {
+      background.style.background = 'url(../assets/扭蛋/window-background.png)';
+      machine.setAttribute('src', '../assets/扭蛋/machine-frame.png');
+      prizes.setAttribute('src', '../assets/扭蛋/window-prizes-shadow.png');
+      joystick.setAttribute('src', '../assets/扭蛋/joystick.png');
+      getImg.setAttribute('src', '../assets/扭蛋/btn-prize-text.png');
+      get.classList.add('style-1');
+    } else if (this.style === '禮物') {
+      background.style.background = 'url(../assets/禮物/window-background.png)';
+      machine.setAttribute('src', '../assets/禮物/machine-frame.png');
+      prizes.setAttribute('src', '../assets/禮物/window-prizes-shadow.png');
+      joystick.setAttribute('src', '../assets/禮物/joystick.png');
+      getImg.setAttribute('src', '../assets/禮物/btn-prize-text.png');
+      get.classList.add('style-2');
+    } else if (this.style === '彩球') {
+      background.style.background = 'url(../assets/彩球/window-background.png)';
+      machine.setAttribute('src', '../assets/彩球/machine-frame.png');
+      prizes.setAttribute('src', '../assets/彩球/window-prizes-shadow.png');
+      joystick.setAttribute('src', '../assets/彩球/joystick.png');
+      getImg.setAttribute('src', '../assets/彩球/btn-prize-text.png');
+      get.classList.add('style-3');
+    }
+    
+    banner.style.background = `${this.banner}`;
+    icon.style.background = `${this.icon}`;
   }
 
   swingRight() {
-    const machine = document.querySelector('.container') as HTMLElement;
     const claw = document.querySelector('.claw') as HTMLElement;
 
     this.count = setInterval(() => {
-      this.x += 0.5;
-      claw.style.transform = `translateX(${this.x}px)`;
-      if (this.x > (machine.offsetWidth - this.x)) {
+      this.x += 0.125;
+      claw.style.transform = `translateX(${this.x}vw)`;
+      if (this.x > 55) {
         clearInterval(this.count);
         this.swingLeft();
       };
@@ -101,8 +127,8 @@ export class AppComponent {
     if (this.x < 0) return;
 
     this.count = setInterval(() => {
-      this.x -= 0.5;
-      claw.style.transform = `translateX(${this.x}px)`;
+      this.x -= 0.125;
+      claw.style.transform = `translateX(${this.x}vw)`;
       if (this.x < 0) {
         clearInterval(this.count);
         this.swingRight();
@@ -111,8 +137,9 @@ export class AppComponent {
   }
 
   grab(): void {
+    this.onlyOnce();
+
     clearInterval(this.count);
-    const machine = document.querySelector('.container') as HTMLElement;
     const claw = document.querySelector('.claw') as HTMLElement;
     const clawBar = document.querySelector('.bar-claw') as HTMLElement;
     const armClaw = document.querySelector('.body-claw') as HTMLElement;
@@ -128,26 +155,26 @@ export class AppComponent {
     this.count = setInterval(() => {
       leftClaw.classList.add('clawGrab');
       rightClaw.classList.add('clawGrab');
-      this.y += 0.75;
-      armClaw.style.transform = `translate3d(${0}px,${this.y}px, 0)`;
-      clawBar.style.height = `${this.y + 30}px`;
+      this.y += 0.15;
+      armClaw.style.transform = `translate3d(${0}vw,${this.y}vw, 0)`;
+      clawBar.style.height = `${this.y + 10}vw`;
 
-      if (this.y > (machine.offsetWidth / 4)) { //暫時
+      if (this.y > 27) { //爪子往下到底
         clearInterval(this.count);
         this.getPrize();
         this.count = setInterval(() => {
           leftClaw.classList.remove('clawGrab');
           rightClaw.classList.remove('clawGrab');
-          this.y -= 0.75;
-          armClaw.style.transform = `translate3d(${0}px,${this.y}px, 0)`;
-          clawBar.style.height = `${this.y + 30}px`;
+          this.y -= 0.15;
+          armClaw.style.transform = `translate3d(${0}vw,${this.y}vw, 0)`;
+          clawBar.style.height = `${this.y + 10}vw`;
 
           if (this.y < 0) {
             clearInterval(this.count);
             if (!this.win) this.dropPrize() //未中獎掉禮物
             this.count = setInterval(() => {
-              this.x -= 0.5;
-              claw.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
+              this.x -= 0.15;
+              claw.style.transform = `translate3d(${this.x}vw,${this.y}vw, 0)`;
 
               if (this.x < 0) {
                 leftClaw.classList.add('clawGrab');
@@ -175,7 +202,13 @@ export class AppComponent {
     let prize = document.createElement("div");
     prize.setAttribute('class', 'prize');
     claw.appendChild(prize);
-    prize.style.backgroundImage = "url('../assets/prize-get.png')";
+    if(this.style === '扭蛋') {
+      prize.style.backgroundImage = "url('../assets/扭蛋/prize-get.png')";
+    } else if (this.style === '禮物') {
+      prize.style.backgroundImage = "url('../assets/禮物/prize-get.png')";
+    } else if (this.style === '彩球') {
+      prize.style.backgroundImage = "url('../assets/彩球/prize-get.png')";
+    }
     prize.style.backgroundSize = '13vw';
     prize.style.width = '13vw';
     prize.style.height = '13vw';
@@ -185,27 +218,36 @@ export class AppComponent {
   }
 
   dropPrize(): void {
-    const machine = document.querySelector('.container') as HTMLElement;
     const prize = document.querySelector('.prize') as HTMLElement;
 
     let prizeX = this.x;
     let prizeY = this.y;
     this.count = setInterval(() => {
       if (!this.win) { //未中獎
-        prizeY++;
-        prize.style.transform = `translate3d(${prizeX - this.x}px,${prizeY}px, 0)`;
+        prizeY+=0.2;
+        prize.style.transform = `translate3d(${prizeX - this.x}vw,${prizeY}vw, 0)`;
       } else {  //中獎
-        this.y++;
-        prize.style.transform = `translate3d(${this.x}px,${this.y}px, 0)`;
+        this.y+=0.2;
+        prize.style.transform = `translate3d(${this.x}vw,${this.y}vw, 0)`;
       }
 
-      if (this.y > (machine.offsetWidth / 4) || prizeY > (machine.offsetWidth / 4)) prize.style.display = 'none';
-      if (this.y > (machine.offsetWidth / 1.3)) {
-        prize.style.right = '4vw';
+      if (this.y > 27 || prizeY > 27) prize.style.display = 'none';
+      if (this.y > 75) {
+        prize.style.right = '4.5vw';
         prize.style.display = 'block';
-        if (this.y > machine.offsetWidth * 0.845) this.y--; return;
+        if (this.y > 84.5) this.y-=0.2; return;
       };
     }, 3);
+  }
+
+  onlyOnce() {
+    const machine = document.querySelector('.container') as HTMLElement;
+    const preventRepeat = document.createElement("div");
+    machine.append(preventRepeat);
+    preventRepeat.style.width = '100vw';
+    preventRepeat.style.height = '100vh';
+    preventRepeat.style.zIndex = '3';
+    preventRepeat.style.position = 'absolute';
   }
 
   change() {
